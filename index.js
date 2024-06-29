f92a10d3b15be68f5d18fe128a2ccf00b88d9245
Bridget <bridget.njoki@student.moringaschool.com>
2024-06-30 00:43:18 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(somevalue){
+    const hqBlock = 42;
+    return Math.abs(somevalue - hqBlock)
+}
+const {distanceFromHqInBlocks } = require('../index');
+const{expect} = require("chai");
